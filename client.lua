c3120da7d5c9e5bbf39302dc68697337c2136478
Clatanii <39058565+Clatanii@users.noreply.github.com>
2020-11-27 22:59:51 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -10,15 +10,6 @@ AddEventHandler('WOSA:INSTANCES:UPDATE', function(player, data, sender)
 	end
 end)
 
-RegisterNetEvent('WOSA:INSTANCES:TOGGLE_VEHICLE')
-AddEventHandler('WOSA:INSTANCES:TOGGLE_VEHICLE', function()
-	if SCRIPT.GET_FOR_ENTITY(GetVehiclePedIsIn(PlayerPedId())) == 0 then
-		SCRIPT.SET_FOR_ENTITY(GetVehiclePedIsIn(PlayerPedId()))
-	else
-		SCRIPT.CLEAR_FOR_ENTITY(GetVehiclePedIsIn(PlayerPedId()))
-	end
-end)
-
 RegisterNetEvent('WOSA:INSTANCES:GET_ALL')
 AddEventHandler('WOSA:INSTANCES:GET_ALL', function(data)
 	synced_table = data
@@ -47,9 +38,7 @@ end
 
 -- MAIN LOOP
 CreateThread(function()
-	DecorRegister('INSTANCE', 3)
 	TriggerServerEvent('WOSA:INSTANCES:GET_ALL')
-
 	SCRIPT.SET(PlayerId(), GetPlayerServerId(PlayerId()))
 
 	while true do Wait(500)
